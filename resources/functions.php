78b270ef413c11b8c127a2b17be0e159402c523d
Vinícius Lyra <vinicius.c.lyra@gmail.com>
2022-10-25 13:50:33 -0300

    chore: logout user without the confirmation screen

--- resources/functions.php
+++ resources/functions.php
@@ -100,3 +100,19 @@ Container::getInstance()
  * Start the Carbon Fields
  */
 require get_template_directory() . '/../custom-fields/init.php';
+
+
+
+function logout_without_confirm($action, $result)
+{
+    /**
+     * Allow logout without confirmation
+     */
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) {
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : home_url('/');
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));
+        header("Location: $location");
+        die;
+    }
+}
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
