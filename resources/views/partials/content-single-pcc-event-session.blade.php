ad92b3c4d8f30cb3165437b02324bbcb77a74070
Vinícius Lyra <vinicius.c.lyra@gmail.com>
2022-12-08 18:57:45 -0300

    chore: remove open collective embed

--- resources/views/partials/content-single-pcc-event-session.blade.php
+++ resources/views/partials/content-single-pcc-event-session.blade.php
@@ -1,27 +1,5 @@
-@php
-$user_id = get_current_user_id();
-$user_is_external_user = user_has_role($user_id, 'external_user');
-$user_has_event = user_has_event($user_id, get_the_ID());
-@endphp
-
 <div class="entry-content" id="content">
-  @if(get_post_meta(get_the_ID(), 'pcc_event_oc_paid_event', true) && (!is_user_logged_in() || (is_user_logged_in() && $user_is_external_user && !$user_has_event)))
-    @if($oc_content = get_post_meta(get_the_ID(), 'pcc_event_oc_content_before_link', true))
-      {!! $oc_content !!}
-    @endif
-    @if($oc_event_url = get_post_meta(get_the_ID(), 'pcc_event_oc_event_link', true))
-      @php
-      $oc_event_embed_url = get_post_meta(get_the_ID(), 'pcc_event_oc_event_embed_link', true);
-      @endphp
-      @if($oc_event_embed_url && !empty($oc_event_embed_url))
-        <p><iframe src="{{$oc_event_embed_url}}" style="width: 100%; height: 130vh; border: none;"></iframe></p>
-      @else
-        <p><a class="payment" href="{{$oc_event_url}}" target="_blank">{{ __('Payment', 'pcc') }}</a></p>
-      @endif
-    @endif
-  @else
-    @content
-  @endif
+  @content
   @if(!empty(SinglePccEvent::eventParticipants()))
   <div class="wp-block-columns has-2-columns">
     <div class="wp-block-column">
