26c4d0b63e4c58dc0f351f7c7aeeb9c139bbb9c5
pedro.castro <pedro.castro@coopersystem.com.br>
2022-12-13 11:04:04 -0300

    feat: add price into single event page when event type is course or past course

--- resources/views/partials/event-meta.blade.php
+++ resources/views/partials/event-meta.blade.php
@@ -7,4 +7,7 @@
     <p class="address" translate="no">@svg('location', ['aria-hidden' => 'true']) {!! str_replace('<p translate="no" class="address">', '', $event_venue) !!}
     @endif
   @endif
+  @if($post->post_type == 'pcc-event' && ($event_program == 'course' || $event_program == 'past_course'))
+    <p class="price">@svg('price', ['aria-hidden' => 'true']) {!! $event_price ?? 'Free' !!}</p>
+  @endif
 </div>
